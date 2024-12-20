7e7c9aaae3a7062455d78ee48549291747470736
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2024-12-20 18:13:45 +0530

    added todays practice problems

--- src/day021224/CountDigitsInNumber.java
+++ src/day021224/CountDigitsInNumber.java
@@ -2,6 +2,11 @@ package day021224;
 
 public class CountDigitsInNumber {
     public static void main(String[] args) {
-
+        int n=89583499;
+        int count=0;
+        for(int i=n;i>0;i=i/10){
+            count++;
+        }
+        System.out.println(count);
     }
 }
