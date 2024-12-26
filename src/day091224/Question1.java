eaa38e4bfe2fa9285b27c29964a37f293d86ab89
Rahul Bhutaiya <rahulbhutaiya13@gmail.com>
2024-12-26 18:01:41 +0530

    added todays practice problems

---
+++ src/day091224/Question1.java
@@ -0,0 +1,13 @@
+package day091224;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int n=6;
+        if(n%2==0){
+            System.out.println(n);
+        }
+        else{
+            System.out.println(n*2);
+        }
+    }
+}
